20b38f19c26cd9e53eb72b4e8d2c8ed41a33515b
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-16 23:47:17 +0900

    fat: show relative timestamp for sensors

--- components/NatureDeviceCard.vue
+++ components/NatureDeviceCard.vue
@@ -18,7 +18,9 @@
               :class="[item.class, item.icon]"
               :aria-label="item.label"
             />
-            <span class="text-right pl-2 pr-1" v-text="item.value" />
+            <UTooltip :text="`${item.ago}更新`">
+              <span class="text-right pl-2 pr-1" v-text="item.value" />
+            </UTooltip>
             <span class="text-center" v-text="item.unit" />
           </template>
         </div>
@@ -28,12 +30,16 @@
 </template>
 
 <script setup lang="ts">
+import { type UseTimeAgoUnitNamesDefault, formatTimeAgo } from "@vueuse/core";
 import type { NatureDeviceDetail } from "~/utils/natureTypes";
 
 const props = defineProps<{
   device: NatureDeviceDetail;
 }>();
 
+const now = useNow({
+  interval: 30_000,
+});
 const items = computed(() => {
   const events = props.device.newest_events;
   return [
@@ -43,6 +49,7 @@ const items = computed(() => {
       label: "室温",
       value: `${events.te.val}`,
       unit: "\u00BAC",
+      timestamp: events.te.created_at,
     },
     events.hu && {
       class: "text-blue-400",
@@ -50,6 +57,7 @@ const items = computed(() => {
       label: "湿度",
       value: `${events.hu.val}`,
       unit: "%",
+      timestamp: events.hu.created_at,
     },
     events.il && {
       class: "text-yellow-400",
@@ -57,7 +65,31 @@ const items = computed(() => {
       label: "明るさ",
       value: `${events.il.val}`,
       unit: "lx",
+      timestamp: events.il.created_at,
     },
-  ].filter((v): v is NonNullable<typeof v> => !!v);
+  ]
+    .filter((v): v is NonNullable<typeof v> => !!v)
+    .map((item) => ({
+      ...item,
+      ago: formatTimeAgo<UseTimeAgoUnitNamesDefault>(
+        new Date(item.timestamp),
+        {
+          messages: {
+            year: (n: number) => `${n}年`,
+            month: (n: number) => `${n}ヶ月`,
+            week: (n: number) => `${n}週間`,
+            day: (n: number) => `${n}日`,
+            hour: (n: number) => `${n}時間`,
+            minute: (n: number) => `${n}分`,
+            second: (n: number) => `${n}秒`,
+            future: (v: string) => `${v}後に`,
+            past: (v: string) => `${v}前に`,
+            invalid: "無効な時刻に",
+            justNow: "たった今",
+          },
+        },
+        now.value
+      ),
+    }));
 });
 </script>
