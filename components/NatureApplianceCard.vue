eec287012dfa22ffb654c67ad9517ba1662490ad
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-17 21:27:55 +0900

    feat(ApplianceCard): implement ApplianceCard

--- components/NatureApplianceCard.vue
+++ components/NatureApplianceCard.vue
@@ -1,15 +1,42 @@
 <template>
-  <UCard>
-    <template #header>
-      {{ appliance.nickname }}
-    </template>
-  </UCard>
+  <ULink :to="`/appliances/${appliance.id}`">
+    <UCard>
+      <div class="flex flex-row items-center gap-4">
+        <div class="flex-1 flex flex-col items-center gap-4">
+          <div class="w-24 h-24 dark:text-gray-200" :class="icon" />
+          <div v-text="appliance.nickname" />
+        </div>
+        <div
+          class="flex-none w-20 flex flex-col items-end justify-between self-stretch"
+        >
+          <div
+            v-if="status.indicator"
+            class="flex-none rounded-full w-2 h-2"
+            :class="status.indicator.class"
+            :aria-label="status.indicator.label"
+          />
+          <div class="flex-1" />
+          <div
+            v-if="status.settings"
+            class="flex-none text-sm font-bold"
+            :class="status.settings.class"
+          >
+            <span v-text="status.settings.label" />
+            <span class="pl-[0.25em]" v-if="status.settings.unit" v-text="status.settings.unit" />
+          </div>
+        </div>
+      </div>
+    </UCard>
+  </ULink>
 </template>
 
 <script setup lang="ts">
 import type { NatureAppliance } from "~/utils/natureTypes";
 
-defineProps<{
+const props = defineProps<{
   appliance: NatureAppliance;
 }>();
+
+const icon = computed(() => natureIconToClass(props.appliance.image));
+const status = useNatureApplianceStatus(() => props.appliance);
 </script>
---
+++ utils/useNatureApplianceStatus.ts
@@ -0,0 +1,90 @@
+import { type NatureAppliance } from "./natureTypes";
+
+export type NatureApplianceStatusIndicatorType = "ON" | "OFF";
+
+export interface NatureApplianceStatusIndicator {
+  readonly type: NatureApplianceStatusIndicatorType;
+  readonly class: string;
+  readonly label: string;
+}
+
+export interface NatureApplianceStatusSettings {
+  readonly class: string;
+  readonly label: string;
+  readonly unit: string | null;
+}
+
+export interface NatureApplianceStatus {
+  readonly indicator: NatureApplianceStatusIndicator | null;
+  readonly settings: NatureApplianceStatusSettings | null;
+}
+
+function getNatureApplianceIndicatorType(
+  appliance: NatureAppliance | null | undefined
+): NatureApplianceStatusIndicatorType | undefined {
+  switch (appliance?.type) {
+    case "AC": {
+      const button = appliance.settings?.button;
+      return button != null && button !== "power-off" ? "ON" : "OFF";
+    }
+
+    case "LIGHT":
+      return appliance.light.state.power === "on" ? "ON" : "OFF";
+  }
+}
+
+export function getNatureApplianceStatus(
+  appliance: NatureAppliance | null | undefined
+): NatureApplianceStatus {
+  const indicatorType: NatureApplianceStatusIndicatorType | undefined =
+    getNatureApplianceIndicatorType(appliance);
+  const indicator: NatureApplianceStatusIndicator | null = indicatorType
+    ? {
+        type: indicatorType,
+        class: indicatorType === "ON" ? "bg-green-400" : "bg-gray-400",
+        label: indicatorType === "ON" ? "ON" : "OFF",
+      }
+    : null;
+
+  let settings: NatureApplianceStatusSettings | null = null;
+  switch (appliance?.type) {
+    case "AC": {
+      const acSettings = appliance.settings;
+      if (acSettings && indicator?.type === "ON") {
+        const colorClass =
+          acSettings.mode === "warm"
+            ? "text-orange-400"
+            : acSettings.mode === "cool"
+            ? "text-blue-400"
+            : "";
+        const label = colorClass
+          ? acSettings.temp
+          : { auto: "自動", dry: "除湿", blow: "送風", cool: "", warm: "" }[
+              acSettings.mode
+            ];
+        const unit = colorClass
+          ? `\u00BA${acSettings.temp_unit.toUpperCase()}`
+          : null;
+        settings = {
+          class: colorClass,
+          label,
+          unit,
+        };
+      }
+      break;
+    }
+  }
+
+  return {
+    indicator,
+    settings,
+  };
+}
+
+export function useNatureApplianceStatus(
+  appliance: MaybeRefOrGetter<NatureAppliance | null | undefined>
+) {
+  return computed<NatureApplianceStatus>(() =>
+    getNatureApplianceStatus(toValue(appliance))
+  );
+}
