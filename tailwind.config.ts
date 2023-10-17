26eb662f116f7ca3cf8c7d28008dba3744b1c42d
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-17 21:26:24 +0900

    chore: add ph icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
 import type { Config } from "tailwindcss";
 import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
 
-console.log("TAILWIND.CONFIG.TS");
-
 export default {
   content: [],
   plugins: [
     iconsPlugin({
-      collections: getIconCollections(["mingcute", "solar"]),
+      collections: getIconCollections(["mingcute", "ph", "solar"]),
     }),
   ],
 } satisfies Config;
