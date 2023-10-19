a25ebf1e2f4f9a42c0b25650e5c0cabdb48e674b
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-20 00:01:07 +0900

    chore(appliance): better mobile layout

--- pages/appliances/[id].vue
+++ pages/appliances/[id].vue
@@ -4,16 +4,16 @@
     class="flex flex-col gap-8 py-4 w-full items-center"
   >
     <div
-      class="relative h-48 flex-none flex flex-row self-stretch justify-between items-center gap-2"
+      class="relative h-32 sm:h-48 flex-none flex flex-row self-stretch justify-between items-center gap-2"
     >
-      <div class="flex-1 text-3xl text-center" v-text="appliance.nickname" />
+      <div class="flex-1 text-3xl max-sm:px-6 sm:text-center" v-text="appliance.nickname" />
       <div
-        class="absolute inset-0 m-auto left-auto flex-none w-64 h-64 dark:text-gray-200/50"
+        class="absolute inset-0 m-auto left-auto flex-none w-48 h-48 sm:w-64 sm:h-64 dark:text-gray-200/50 -z-10"
         :class="natureIconToClass(appliance.image)"
       />
     </div>
     <hr class="w-full dark:border-gray-700" />
-    <div class="max-w-xs w-full">
+    <div class="max-w-[240px] w-full">
       <NatureApplianceControlLight
         v-if="appliance.type === 'LIGHT'"
         :appliance="appliance"
