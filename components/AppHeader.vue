44429b584a3b0f0195798b7ddae69feaac30a07e
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-20 00:06:31 +0900

    fix(Header): set z-index

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-16">
     <div
-      class="fixed top-0 left-0 right-0 h-16 flex px-4 gap-4 items-center justify-end border-b dark:border-gray-600 dark:bg-gray-800"
+      class="fixed z-10 top-0 left-0 right-0 h-16 flex px-4 gap-4 items-center justify-end border-b dark:border-gray-600 dark:bg-gray-800"
     >
       <ULink to="/" class="flex-none text-2xl font-bold">Luonto</ULink>
       <div class="flex-1" />
