99b7df0872acc32e0266a37c1de61572b4eee55d
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-22 01:16:18 +0900

    feat: pre-define all light buttons

--- components/NatureApplianceControlLight.vue
+++ components/NatureApplianceControlLight.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="w-full flex flex-col gap-8">
+  <div class="w-full max-w-[220px] flex flex-col gap-8 items-center mx-auto">
     <div class="relative w-full aspect-square">
       <div
         class="absolute inset-0 w-full h-full -z-1 border-[40px] rounded-full dark:border-gray-800"
       />
-      <template v-for="button in presetButtons" v-key="button.name">
+      <template
+        v-for="{
+          name,
+          placeClass,
+          color,
+          variant,
+          label,
+          image,
+        } in circleButtons"
+        v-key="name"
+      >
         <div
-          v-if="buttons.includes(button.name)"
+          v-if="allButtonNames.includes(name)"
           class="absolute m-auto inset-0 flex items-center justify-center"
-          :class="button.placeClass"
+          :class="placeClass"
         >
           <UButton
             class="rounded-full"
             size="lg"
-            :color="button.color"
-            :variant="button.variant"
-            :aria-label="button.label"
-            :icon="natureIconToClass(button.image)"
+            :color="color"
+            :variant="variant"
+            :aria-label="label"
+            :icon="natureIconToClass(image)"
             :disabled="submitting"
-            @click="send(button.name)"
+            @click="send(name)"
           />
         </div>
       </template>
     </div>
     <div class="w-full grid grid-flow-col auto-cols-min gap-4">
-      <template
-        v-for="{ name, image, label } in appliance.light.buttons"
-        v-key="name"
-      >
+      <template v-for="{ name, image, label } in otherButtons" v-key="name">
         <UButton
-          v-if="
-            name !== 'on' &&
-            name !== 'bright-down' &&
-            name !== 'bright-up' &&
-            name !== 'colortemp-down' &&
-            name !== 'colortemp-up'
-          "
           class="rounded-full"
           size="lg"
           color="gray"
@@ -62,52 +62,83 @@ const props = defineProps<{
   onSend: (promise: Promise<unknown>) => void;
 }>();
 
-const presetButtons = [
-  {
-    name: "on",
-    image: "ico_on",
-    label: "ON",
-    variant: "solid",
-    color: "gray",
-    placeClass: "",
-  },
-  {
-    name: "bright-up",
-    image: "ico_lightup",
-    label: "明るくする",
-    variant: "link",
-    color: "gray",
-    placeClass: "bottom-auto",
-  },
-  {
-    name: "bright-down",
-    image: "ico_lightdown",
-    label: "暗くする",
-    variant: "link",
-    color: "gray",
-    placeClass: "top-auto",
-  },
-  {
-    name: "colortemp-down",
-    image: "ico_colortemp_down",
-    label: "色温度を下げる",
-    variant: "link",
-    color: "blue",
-    placeClass: "right-auto",
-  },
-  {
-    name: "colortemp-up",
-    image: "ico_colortemp_up",
-    label: "色温度を上げる",
-    variant: "link",
-    color: "orange",
-    placeClass: "left-auto",
-  },
-] as const;
-
-const buttons = computed(() =>
+const allButtonNames = computed(() =>
   props.appliance.light.buttons.map((b) => b.name)
 );
+
+const circleButtons = computed(() => {
+  return (
+    [
+      {
+        name: "on",
+        image: "ico_on",
+        label: "ON",
+        variant: "solid",
+        color: "gray",
+        placeClass: "",
+      },
+      {
+        name: "bright-up",
+        image: "ico_lightup",
+        label: "明るくする",
+        variant: "link",
+        color: "gray",
+        placeClass: "bottom-auto",
+      },
+      {
+        name: "bright-down",
+        image: "ico_lightdown",
+        label: "暗くする",
+        variant: "link",
+        color: "gray",
+        placeClass: "top-auto",
+      },
+      {
+        name: "colortemp-down",
+        image: "ico_colortemp_down",
+        label: "色温度を下げる",
+        variant: "link",
+        color: "blue",
+        placeClass: "right-auto",
+      },
+      {
+        name: "colortemp-up",
+        image: "ico_colortemp_up",
+        label: "色温度を上げる",
+        variant: "link",
+        color: "orange",
+        placeClass: "left-auto",
+      },
+    ] as const
+  ).filter((b) => allButtonNames.value.includes(b.name));
+});
+
+const otherButtons = computed(() => {
+  return (
+    [
+      {
+        name: "off",
+        image: "ico_off",
+        label: "OFF",
+      },
+      {
+        name: "on-100",
+        image: "ico_light_all",
+        label: "全灯",
+      },
+      {
+        name: "on-favorite",
+        image: "ico_light_favorite",
+        label: "お気に入り",
+      },
+      {
+        name: "colortemp-down",
+        image: "ico_light_night",
+        label: "常夜灯",
+      },
+    ] as const
+  ).filter((b) => allButtonNames.value.includes(b.name));
+});
 
 const send = (button: NatureApplianceLightButtonName): void => {
   props.onSend(
--- pages/appliances/[id].vue
+++ pages/appliances/[id].vue
@@ -6,14 +6,17 @@
     <div
       class="relative h-32 sm:h-48 flex-none flex flex-row self-stretch justify-between items-center gap-2"
     >
-      <div class="flex-1 text-3xl max-sm:px-6 sm:text-center" v-text="appliance.nickname" />
+      <div
+        class="flex-1 text-3xl max-sm:px-6 sm:text-center"
+        v-text="appliance.nickname"
+      />
       <div
         class="absolute inset-0 m-auto left-auto flex-none w-48 h-48 sm:w-64 sm:h-64 dark:text-gray-200/50 -z-10"
         :class="natureIconToClass(appliance.image)"
       />
     </div>
     <hr class="w-full dark:border-gray-700" />
-    <div class="max-w-[240px] w-full">
+    <div class="max-w-xs px-4 w-full">
       <NatureApplianceControlLight
         v-if="appliance.type === 'LIGHT'"
         :appliance="appliance"
