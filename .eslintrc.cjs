2e605da1b1f5560e5aa9bdfacc09dad998ff9e94
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-28 22:34:59 +0900

    dx: migrate to eslint-config-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config", "@vue/eslint-config-prettier"],
+  extends: ["@nuxt", "prettier"],
   parserOptions: {
     parser: {
       mts: "@typescript-eslint/parser",
