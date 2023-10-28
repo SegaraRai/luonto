4fb4fbfbfcdec04e24e9c681b5d58ea0b64dc676
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-28 15:50:47 +0900

    feat: improve home headings

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col gap-4 py-4 w-full">
-    <h2>デバイス</h2>
+    <h2 class="text-xl">デバイス</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-4">
       <template v-for="device in data?.devices" :key="device.id">
         <NatureDeviceCard :device="device" />
       </template>
     </div>
-    <h2>コントロール</h2>
+    <h2 class="text-xl">コントロール</h2>
     <div
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-4"
     >
