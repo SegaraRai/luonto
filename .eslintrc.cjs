24f3e01d3e42746e135c7b74c5f6cac64d56b1c6
SegaraRai <SegaraRai@users.noreply.github.com>
2023-10-29 12:44:46 +0900

    dx: allow multiple template root

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,18 @@
-module.exports = {
+// @ts-check
+
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
   root: true,
   extends: ["@nuxt", "prettier"],
   parserOptions: {
+    // @ts-ignore
     parser: {
       mts: "@typescript-eslint/parser",
     },
   },
-};
+  rules: {
+    // Vue 3 allows multiple root elements
+    "vue/no-multiple-template-root": "off",
+  },
+});
