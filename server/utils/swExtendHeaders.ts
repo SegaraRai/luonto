992b4b2cf9cf12863046ae0b694d652728b8cc42
SegaraRai <SegaraRai@users.noreply.github.com>
2023-11-28 01:45:34 +0900

    fix: fix issue with sign-in

--- server/utils/swExtendHeaders.ts
+++ server/utils/swExtendHeaders.ts
@@ -7,17 +7,22 @@ declare global {
 
 export function extendHeaders(): void {
   if (globalThis.__REQ_RES_TWEAKED__ || !isSW) {
+    console.log(
+      "Skip tweaking Request and Response classes",
+      globalThis.__REQ_RES_TWEAKED__,
+      !isSW
+    );
     return;
   }
 
-  console.debug("Tweaking Request and Response classes");
+  console.log("Tweaking Request and Response classes");
 
   globalThis.__REQ_RES_TWEAKED__ = true;
 
   globalThis.Request = class Request extends globalThis.Request {
-    readonly #headersOverride: Headers | undefined;
+    readonly #headersOverride?: Headers | undefined;
 
-    constructor(input: RequestInfo | URL, init: RequestInit | undefined) {
+    constructor(input: RequestInfo | URL, init?: RequestInit | undefined) {
       super(input, init);
       this.#headersOverride = init?.headers && new Headers(init.headers);
     }
@@ -28,11 +33,30 @@ export function extendHeaders(): void {
   };
 
   globalThis.Response = class Response extends globalThis.Response {
-    readonly #headersOverride: Headers | undefined;
+    static json(data: unknown, init?: ResponseInit): globalThis.Response {
+      const base = super.json(data, init);
+
+      // as `Response.headers` is mutable, we must always override it
+      const headers = new Headers(init?.headers);
+      const contentType = base.headers.get("content-type");
+      if (contentType != null) {
+        headers.set("content-type", contentType);
+      } else {
+        headers.delete("content-type");
+      }
+
+      return new Response(base.body, {
+        ...base,
+        headers,
+      });
+    }
+
+    readonly #headersOverride?: Headers | undefined;
 
     constructor(body?: BodyInit | null, init?: ResponseInit) {
       super(body, init);
-      this.#headersOverride = init?.headers && new Headers(init.headers);
+      // as `Response.headers` is mutable, we must always override it
+      this.#headersOverride = new Headers(init?.headers);
     }
 
     get headers(): Headers {
