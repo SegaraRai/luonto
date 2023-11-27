150920da691b2b4bea112d85164be352a647d27a
SegaraRai <SegaraRai@users.noreply.github.com>
2023-11-27 23:49:13 +0900

    feat: better loading screen

--- pages/loading.vue
+++ pages/loading.vue
@@ -1,9 +1,16 @@
 <template>
-  <p>
-    Service Worker をインストールしています…
-    <br />
-    インストール後、アプリは自動的に起動します。しばらくお待ちください。
-  </p>
+  <div class="flex flex-col items-center justify-center w-full h-[100dvh]">
+    <UCard class="w-full sm:max-w-lg">
+      <div class="flex flex-col gap-4">
+        <p>
+          Luonto をインストールしています…
+          <br />
+          インストール後、アプリは自動的に起動します。しばらくお待ちください。
+        </p>
+        <UProgress animation="carousel" />
+      </div>
+    </UCard>
+  </div>
 </template>
 
 <script setup lang="ts">
