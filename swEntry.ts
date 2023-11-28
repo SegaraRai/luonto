95f0f18fc7bad74dd86e1bf6fc2682daf525c7b3
SegaraRai <SegaraRai@users.noreply.github.com>
2023-11-29 01:28:22 +0900

    perf: do concurrent request on precache

--- swEntry.ts
+++ swEntry.ts
@@ -8,7 +8,7 @@ import {
   PrecacheStrategy,
 } from "workbox-precaching";
 import { Router } from "workbox-routing";
-import { NetworkFirst } from "workbox-strategies";
+import { NetworkFirst, type Strategy } from "workbox-strategies";
 import { nitroApp } from "#internal/nitro/app";
 import { isPublicAssetURL } from "#internal/nitro/virtual/public-assets";
 import { anonymizeData } from "./server/utils/anonymizeDetailCache";
@@ -17,6 +17,8 @@ import {
   createCookieForRequest,
   storeCookiesFromResponse,
 } from "./server/utils/swCookieStorage";
+
+const MAX_CONCURRENCY = 6;
 
 type AssetManifestEntry = [
   filepath: string,
@@ -106,6 +108,68 @@ function tweakPrecacheStrategyToUseAssetArchive(
   };
 }
 
+function tweakPrecacheControllerToRequestConcurrently(
+  controller: PrecacheController,
+  maxConcurrency: number
+): void {
+  const promiseSet: Set<Promise<void>> = new Set();
+  let rejectedPromise: Promise<unknown> | undefined;
+
+  const waitForAll = async (): Promise<void> => {
+    await Promise.all(promiseSet);
+    await rejectedPromise;
+  };
+
+  const orgHandleAll = controller.strategy.handleAll;
+  const handleInternal = async function (
+    this: Strategy,
+    params: Parameters<typeof orgHandleAll>[0],
+    wait = false
+  ): Promise<void> {
+    while (promiseSet.size >= maxConcurrency) {
+      await Promise.any(promiseSet);
+    }
+
+    await rejectedPromise;
+
+    const promise = Promise.all(orgHandleAll.call(this, params))
+      .catch((error) => {
+        rejectedPromise = Promise.reject(error);
+      })
+      .then(() => {
+        promiseSet.delete(promise);
+      });
+
+    promiseSet.add(promise);
+
+    if (wait) {
+      await waitForAll();
+    }
+  };
+
+  controller.strategy.handleAll = function (
+    params
+  ): [Promise<Response>, Promise<void>] {
+    if ((params as { event: Event }).event?.type !== "install") {
+      return orgHandleAll.call(this, params);
+    }
+
+    const entries: [url: string, cacheKey: string][] = Array.from(
+      controller.getURLsToCacheKeys().entries()
+    );
+    const index = entries.findIndex(
+      ([url, cacheKey]) =>
+        cacheKey ===
+          (params as { params?: { cacheKey?: string } }).params?.cacheKey ||
+        (params as { request?: Request }).request?.url === url
+    );
+    const shouldWait = index < 0 || index === entries.length - 1;
+    console.log(params, index, shouldWait);
+    const promise = handleInternal.call(this, params, shouldWait);
+    return [promise.then(() => Response.error()), promise];
+  };
+}
+
 // nitro
 async function handleEvent(url: URL, event: FetchEvent): Promise<Response> {
   let body: ArrayBuffer | undefined;
@@ -146,6 +210,10 @@ tweakPrecacheStrategyToUseAssetArchive(
   self.__WBX_ASSET_ARCHIVE_PATH,
   self.__WBX_ASSET_ARCHIVE_INTEGRITY,
   self.__WBX_ASSET_MANIFEST
+);
+tweakPrecacheControllerToRequestConcurrently(
+  precacheController,
+  MAX_CONCURRENCY
 );
 precacheController.precache(self.__WB_MANIFEST);
 
