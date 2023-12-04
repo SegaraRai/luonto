1191fa1042b99e0b7d84a8051c1fe7e8d81607dc
SegaraRai <SegaraRai@users.noreply.github.com>
2023-12-05 03:48:34 +0900

    feat: show error if service worker initialization failed

--- swEntry.ts
+++ swEntry.ts
@@ -226,104 +226,120 @@ async function handleEvent(url: URL, event: FetchEvent): Promise<Response> {
   return resWithoutSetCookie;
 }
 
-// workbox
-const precacheController = new PrecacheController();
-tweakPrecacheStrategyToUseAssetArchive(
-  precacheController.strategy as PrecacheStrategy,
-  self.__WBX_ASSET_ARCHIVE_PATH,
-  self.__WBX_ASSET_ARCHIVE_INTEGRITY,
-  self.__WBX_ASSET_MANIFEST
-);
-tweakPrecacheControllerToRequestConcurrently(
-  precacheController,
-  MAX_CONCURRENCY
-);
-precacheController.precache(self.__WB_MANIFEST);
+// start
+async function start() {
+  // workbox
+  const precacheController = new PrecacheController();
+  tweakPrecacheStrategyToUseAssetArchive(
+    precacheController.strategy as PrecacheStrategy,
+    self.__WBX_ASSET_ARCHIVE_PATH,
+    self.__WBX_ASSET_ARCHIVE_INTEGRITY,
+    self.__WBX_ASSET_MANIFEST
+  );
+  tweakPrecacheControllerToRequestConcurrently(
+    precacheController,
+    MAX_CONCURRENCY
+  );
+  precacheController.precache(self.__WB_MANIFEST);
 
-const precacheRoute = new PrecacheRoute(precacheController);
+  const precacheRoute = new PrecacheRoute(precacheController);
 
-const router = new Router();
-router.registerRoute(precacheRoute);
+  const router = new Router();
+  router.registerRoute(precacheRoute);
 
-router.setDefaultHandler(new NetworkFirst());
+  router.setDefaultHandler(new NetworkFirst());
 
-router.addCacheListener();
+  router.addCacheListener();
 
-// event handlers
-self.addEventListener("fetch", (event): void => {
-  const { request } = event;
-  const { method, url: strURL } = request;
-  const url = new URL(strURL);
-  if (url.protocol !== "https:" && url.protocol !== "http:") {
-    // e.g. chrome-extension:
-    return;
-  }
-
-  // here we determine whether the resource at the specified URL **should be fetched from the remote or not**
-  // this is NOT about whether the asset should be retrieved from the cache
-  // all resources that are not handled here will be handled by Nuxt, so any assets being delivered must be processed here
-  if (
-    url.hostname === "api.nature.global" ||
-    url.hostname === "cloudflareinsights.com" ||
-    url.hostname === "static.cloudflareinsights.com" ||
-    isPublicAssetURL(url.pathname) ||
-    url.pathname === "/sw.js" ||
-    url.pathname.startsWith("/server.")
-  ) {
-    let newRequestPromise: Promise<Request> | undefined;
-
-    // anonymize data before sending metrics
-    if (method === "POST" && url.pathname === "/cdn-cgi/rum") {
-      const orgRequest = request.clone();
-      newRequestPromise = (async () =>
-        new Request(request, {
-          ...request,
-          body: await anonymizeData(await request.text(), (from, to): void => {
-            console.info(`Anonymized "${from}" to "${to}"`);
-          }),
-        }))().catch(() => orgRequest);
-    }
-
-    // fetch using workbox
-    const response =
-      newRequestPromise?.then(
-        (request) =>
-          // if request is a promise, it's modified by us so we have to send manually (we cannot use browser's default fetch) if workbox didn't handle it
-          router.handleRequest({ event, request }) ?? fetch(request)
-      ) ?? router.handleRequest({ event, request });
-
-    // we didn't modify the request and workbox didn't handle the request, use browser's default fetch
-    if (!response) {
+  // event handlers
+  self.addEventListener("fetch", (event): void => {
+    const { request } = event;
+    const { method, url: strURL } = request;
+    const url = new URL(strURL);
+    if (url.protocol !== "https:" && url.protocol !== "http:") {
+      // e.g. chrome-extension:
       return;
     }
 
-    // we cannot collect anonymized data from Nature API calls here as they are `fetch`ed in Service Worker and therefore do not come here ("fetch" event)
+    // here we determine whether the resource at the specified URL **should be fetched from the remote or not**
+    // this is NOT about whether the asset should be retrieved from the cache
+    // all resources that are not handled here will be handled by Nuxt, so any assets being delivered must be processed here
+    if (
+      url.hostname === "api.nature.global" ||
+      url.hostname === "cloudflareinsights.com" ||
+      url.hostname === "static.cloudflareinsights.com" ||
+      isPublicAssetURL(url.pathname) ||
+      url.pathname === "/sw.js" ||
+      url.pathname.startsWith("/server.")
+    ) {
+      let newRequestPromise: Promise<Request> | undefined;
+
+      // anonymize data before sending metrics
+      if (method === "POST" && url.pathname === "/cdn-cgi/rum") {
+        const orgRequest = request.clone();
+        newRequestPromise = (async () =>
+          new Request(request, {
+            ...request,
+            body: await anonymizeData(
+              await request.text(),
+              (from, to): void => {
+                console.info(`Anonymized "${from}" to "${to}"`);
+              }
+            ),
+          }))().catch(() => orgRequest);
+      }
+
+      // fetch using workbox
+      const response =
+        newRequestPromise?.then(
+          (request) =>
+            // if request is a promise, it's modified by us so we have to send manually (we cannot use browser's default fetch) if workbox didn't handle it
+            router.handleRequest({ event, request }) ?? fetch(request)
+        ) ?? router.handleRequest({ event, request });
+
+      // we didn't modify the request and workbox didn't handle the request, use browser's default fetch
+      if (!response) {
+        return;
+      }
+
+      // we cannot collect anonymized data from Nature API calls here as they are `fetch`ed in Service Worker and therefore do not come here ("fetch" event)
+
+      event.respondWith(
+        response.catch((error) =>
+          createErrorResponse(error, request, "workbox")
+        )
+      );
+
+      return;
+    }
 
     event.respondWith(
-      response.catch((error) => createErrorResponse(error, request, "workbox"))
+      handleEvent(url, event).catch((error) =>
+        createErrorResponse(error, request, "workbox")
+      )
     );
+  });
 
-    return;
-  }
+  self.addEventListener("install", (): void => {
+    // we don't have to call `precacheController.install()` here as it's handled by `precache()` call above
+    // https://github.com/GoogleChrome/workbox/blob/v7.0.0/packages/workbox-precaching/src/PrecacheController.ts#L111-L115
+    self.skipWaiting();
+  });
 
-  event.respondWith(
-    handleEvent(url, event).catch((error) =>
-      createErrorResponse(error, request, "workbox")
-    )
-  );
+  self.addEventListener("activate", (event): void => {
+    // we don't have to call `precacheController.activate()` here as it's handled by `precache()` call above
+    // https://github.com/GoogleChrome/workbox/blob/v7.0.0/packages/workbox-precaching/src/PrecacheController.ts#L111-L115
+    event.waitUntil(self.clients.claim());
+  });
+
+  // extend Request and Response classes
+  extendHeaders();
+}
+
+start().catch((error) => {
+  console.error("Failed to initialize Service Worker", error);
+
+  self.addEventListener("fetch", (event): void => {
+    event.respondWith(createErrorResponse(error, event.request, "init"));
+  });
 });
-
-self.addEventListener("install", (): void => {
-  // we don't have to call `precacheController.install()` here as it's handled by `precache()` call above
-  // https://github.com/GoogleChrome/workbox/blob/v7.0.0/packages/workbox-precaching/src/PrecacheController.ts#L111-L115
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event): void => {
-  // we don't have to call `precacheController.activate()` here as it's handled by `precache()` call above
-  // https://github.com/GoogleChrome/workbox/blob/v7.0.0/packages/workbox-precaching/src/PrecacheController.ts#L111-L115
-  event.waitUntil(self.clients.claim());
-});
-
-// extend Request and Response classes
-extendHeaders();
