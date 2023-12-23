9263291afb23022bcf3eb43bfb1ab1aebf8678dc
SegaraRai <SegaraRai@users.noreply.github.com>
2023-12-24 01:39:24 +0900

    test: initialize test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import { createNitroSWPreset } from "./nitroSWPreset";
 
 const nitro: {} =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
     ? {}
     : {
         ...createNitroSWPreset({
---
+++ server/utils/serial.test.ts
@@ -0,0 +1,48 @@
+import { test, vi, expect, expectTypeOf } from "vitest";
+import { createSerial } from "./serial";
+
+function createPromise(): [Promise<void>, () => void] {
+  let resolve: () => void;
+  const promise = new Promise<void>((res) => {
+    resolve = res;
+  });
+  return [promise, resolve!];
+}
+
+test("should return function that executes specified function", () => {
+  const fn = vi.fn<[], Promise<void>>().mockResolvedValue(undefined);
+  const serial = createSerial(fn);
+  expectTypeOf(serial).toBeFunction();
+  serial();
+  expect(fn).toHaveBeenCalled();
+});
+
+test("should not call next one until running one finishes", async () => {
+  const resolves: (() => void)[] = [];
+  const serial = createSerial(() => {
+    const [promise, resolve] = createPromise();
+    resolves.push(resolve);
+    return promise;
+  });
+
+  const promises = new Array(20).fill(0).map(() => {
+    const onResolved = vi.fn();
+    return [serial().then(onResolved), onResolved] as const;
+  });
+
+  expect(resolves).toHaveLength(1);
+  expect(promises[0][1]).not.toHaveBeenCalled();
+  expect(promises[1][1]).not.toHaveBeenCalled();
+  resolves[0]();
+  await promises[0][0];
+  expect(promises[0][1]).toHaveBeenCalled();
+  expect(promises[1][1]).not.toHaveBeenCalled();
+
+  expect(resolves).toHaveLength(2);
+  resolves[1]();
+  await promises[1][0];
+  expect(promises[1][1]).toHaveBeenCalled();
+  expect(promises[19][1]).toHaveBeenCalled();
+
+  expect(resolves).toHaveLength(2);
+});
