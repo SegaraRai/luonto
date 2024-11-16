aef560e18c76e416f95fc31dc01969799b7c8004
SegaraRai <SegaraRai@users.noreply.github.com>
2024-11-17 03:47:14 +0900

    fix: cache not invalidated on API POST

--- app/utils/useFetchCacheControlHelper.ts
+++ app/utils/useFetchCacheControlHelper.ts
@@ -4,18 +4,14 @@ export function useFetchCacheControlHelper() {
   const isInitialRequest = ref(true);
   const forceRefreshTargets = ref<InvalidateTarget[]>([]);
 
-  const onRequest = (context: FetchContext): void => {
+  const onRequest = ({ options }: FetchContext): void => {
     if (isInitialRequest.value) {
       isInitialRequest.value = false;
       return;
     }
 
-    const headers = new Headers(
-      typeof context.request === "object" ? context.request.headers : {}
-    );
-
     if (forceRefreshTargets.value.length) {
-      headers.set(
+      options.headers.set(
         "luonto-invalidate-cache",
         forceRefreshTargets.value.join(", ")
       );
@@ -23,13 +19,10 @@ export function useFetchCacheControlHelper() {
     } else {
       // for scheduled updates, get the latest results, even if it takes longer, since they have already been rendered and user interaction is not relevant
       // if caching is disabled for certain APIs (that is, due to user interaction), allow stale responses for other APIs to get results quickly
-      headers.set("luonto-no-stale-cache", "?1");
+      options.headers.set("luonto-no-stale-cache", "?1");
     }
 
-    context.request = new Request(context.request, {
-      cache: "reload",
-      headers,
-    });
+    options.cache = "reload";
   };
 
   return {
